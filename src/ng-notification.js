c02580cb837ceb9b7c9892bf334c88d00de61d84
darul75 <darul75@gmail.com>
2014-05-06 18:04:25 +0200

    new repo

---
+++ src/ng-notification.js
@@ -0,0 +1,14 @@
+(function (angular) {
+'use strict';
+	angular.module('ngMessage', []).directive('notification', function () {		
+
+		return {
+			restrict: 'AE',			
+			link:function (scope, elm, attr) {
+
+				
+			
+			}
+		};
+	});
+})(angular);
